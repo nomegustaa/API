82a8b0c505d7b8b185d242620d3e4f79c957ab3d
nomegustaa <gustavocarvalhodasilva015@gmail.com>
2023-04-13 23:19:32 -0300

    adjusting functions

--- script/script.js
+++ script/script.js
@@ -1,46 +1,58 @@
-const inputCep = document.querySelector('.cep')
+const inputCep = document.querySelector('[data-input-cep]')
 const btnCep = document.querySelector('[data-btn-cep]')
-const resultadoCep = document.querySelector('.resultadoCep')
-btnCep.addEventListener('click', handleClick)
-
-function handleClick(event) {
-  event.preventDefault()
-  const cep = inputCep.value
-  if(cep === '' || cep === 0) {
-    Swal.fire({
-        icon: 'error',
-        iconColor: '#ff0000',
-        showCloseButton: true,
-        showConfirmButton: false,
-        title: 'Digite algum CEP no campo',
-      })
-  }else if(cep.length !== 8){
-    Swal.fire({
-        icon: 'error',
-        iconColor: '#ff0000',
-        showCloseButton: true,
-        showConfirmButton: false,
-        title: 'Cep inválido',
-      })
+const resultadoCep = document.querySelector('[data-resultado-cep]')
+const messaError = document.querySelector('[data-message-error]')
+const submitForm = (event) => {
+  const cepValue = inputCep.value
+  if(event.key === 'Enter'){
+    event.preventDefault()
+    checksInputs(cepValue)
+  }
+}
+const checksInputs = (cep) => {
+  if(cep.trim() === ''){
+    messaError.classList.add('messageActive')
+    messaError.innerHTML = 'Digite algo no campo primeiro!'
+    inputCep.classList.add('inputError')
+    resultadoCep.innerHTML = ``
+  }else if(cep.length > 8 || cep.length < 8){
+    messaError.classList.add('messageActive')
+    messaError.innerHTML = 'Cep inválido, cep geralmente são 8 números'
+    inputCep.classList.add('inputError')
+    resultadoCep.innerHTML = ``
   }
   else{
     buscaCep(cep)
-    console.log(cep)
+    messaError.classList.remove('messageActive')
+    inputCep.classList.remove('inputError')
   }
 }
-function buscaCep(cep) {
-  fetch(`https://viacep.com.br/ws/${cep}/json/`)
+const handleClick = (event) => {
+  event.preventDefault()
+  const cepValue = inputCep.value
+  checksInputs(cepValue) ? buscaCep(cepValue) : ''
+}
+const buscaCep = (cep) => {
+  const apiCep = `https://viacep.com.br/ws/${cep}/json/`
+  fetch(apiCep)
     .then(response => response.json())
     .then((cep => {
         if(cep.erro){
-            resultadoCep.innerHTML = "<p>Não foi possível localizar seu cep!</p>"
+          inputCep.classList.add('inputError')
+          messaError.classList.add('messageActive')
+          messaError.innerHTML = 'Não foi possível localizar seu cep!'
+          resultadoCep.innerHTML = ``
         }else{
-            resultadoCep.innerHTML = ` 
-                <p>Cep : ${cep.cep}</p>
-                <p>Rua : ${cep.logradouro}</p>
-                <p>Complemento : ${cep.complemento}</p>
-                <p>Cidade : ${cep.localidade}</p>
-                <p>Estado : ${cep.uf}</p>`
+          messaError.classList.remove('messageActive')
+          inputCep.classList.remove('inputError')
+            resultadoCep.innerHTML =             
+            ` <p>Cep : ${cep.cep}</p>
+              <p>Rua : ${cep.logradouro}</p>
+              <p>Bairro : ${cep.bairro}</p>
+              <p>Cidade : ${cep.localidade}</p>
+              <p>Estado : ${cep.uf}</p>`
         }
     }))
 }
+inputCep.addEventListener('keypress', submitForm)
+btnCep.addEventListener('click', handleClick)
